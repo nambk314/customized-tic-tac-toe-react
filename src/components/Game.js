5bdce33df66dcd5d76db420073d6f803d7e8ca2e
Nam Bui <buin@cataliahealth.com>
2020-11-27 10:01:47 -0800

    add function to change dimension of the board

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,11 +5,9 @@ import Board from './Board'
 
 
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null))
+    const [dimension, setDimension] = useState(3)
+    const [board, setBoard] = useState(Array(dimension*dimension).fill(null))
     const [xIsNext, setXIsNext] = useState(true)
-
-
-
 
     const handleClick = (i) => {
         let newBoard = [...board]
@@ -18,8 +16,43 @@ const Game = () => {
         setBoard(newBoard)
         setXIsNext(!x)
     }
+
+    const handleInput = (event) => {
+        event.preventDefault()
+        setDimension(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setBoard(Array(dimension*dimension).fill(null))
+    }
+
+    //Dimension may be should be a drop down list
+    const menu = () =>  {
+        return (
+            <div>
+                <button onClick={handleSubmit}>Reset</button>
+                <h2 style = {{margin: '4px 4px'}}>Choose dimension</h2>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type = 'text'
+                        value = {dimension}
+                        name = 'dimension'
+                        placeholder = 'Enter dimension'
+                        onChange = {handleInput}
+                    >
+                    </input>
+                    <button>Change</button>
+                </form>
+            </div>
+        )
+    }
     return (
-        <Board dimension={Math.floor(Math.sqrt(board.length))} squares={board} onClick={handleClick}/>
+        <div>
+            <Board dimension={dimension} squares={board} onClick={handleClick}/>
+            {menu()}
+        </div>
+        
     )
 
 }
